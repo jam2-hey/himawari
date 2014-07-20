f3dbc358a5c38165ca861232fa516e4ef83b9591
John Huang <johnhuang@kkbox.com>
2014-07-20 13:31:03 +0800

    Add node server main.js

---
+++ server/main.js
@@ -0,0 +1,30 @@
+function Himawari() {
+
+	var io,
+		clients = [],
+		backend = null;
+
+	function main() {
+		requires();
+		handleConnections();
+	}
+
+	function requires() {
+		io = require('socket.io')(9999);
+	}
+
+	function handleConnections() {
+		var client_socket = io.of('/client'),
+			backend_socket = io.of('/backend');
+
+		client_socket.on('connection', function(socket) {
+			clients.push(socket);s
+		});
+
+		backend_socket.on('connection', function(socket) {
+			backend = socket;
+		});
+	}
+}
+
+app = new Himawari();
