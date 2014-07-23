c85e2c8712beefadbeb225d615c95b8b4c2273f8
John Huang <johnhuang@kkbox.com>
2014-07-23 18:11:56 +0800

    [Server] Refactor loading process.

--- server/main.js
+++ server/main.js
@@ -1,24 +1,84 @@
 function Himawari() {
 
-    var io, Route, Router,
+    var CONFIG_FILE = './config.json',
+        config,
+        io,
+        Route, Router,
+        router,
+        db,
         sockets = {
             clients: [],
             backend: null
-        }
+        };
+
+    var Promise = require('promise');
+    var fs = require('fs');
 
     main();
 
     function main() {
-        requires();
-        handleConnections();
+        loadConfig()
+            .then(requires)
+            .then(initDatabase)
+            .then(initControllers)
+            .then(initRouter)
+            .then(handleConnections);
+    }
+
+    function loadConfig() {
+        return new Promise(function (fullfill) {
+            fs.readFile(CONFIG_FILE, 'utf8', function (ex, data) {
+                if (ex) throw "Himawari: Connat load config file. Due to '" + ex + "'"
+                config = JSON.parse(data);
+                fullfill();
+            });
+        });
     }
 
     function requires() {
-        // Libraries
-        io = require('socket.io').listen(9999);
-        // Classes
-        Route = require('./class/Route.js');
-        Router = require('./class/Router.js');
+        return new Promise(function (fullfill) {
+            // Libraries
+            io = require('socket.io').listen(9999);
+            _ = require('underscore');
+            mysql = require('mysql');
+            // Classes
+            Route = require('./class/Route.js');
+            Router = require('./class/Router.js');
+            // Controller
+            Member = require('./controller/members.js');
+            fullfill();
+        });
+    }
+
+    function initDatabase() {
+        return new Promise(function (fullfill) {
+            fs.readFile(config.files.db_config, 'utf8', function (ex, data) {
+                var db_config  = JSON.parse(data);
+                db = mysql.createConnection(db_config);
+                db.connect(function (ex) {
+                    if (ex) {
+                        throw "Himawari: Cannot connect database. Due to '" + ex.stack + "'";
+                    }
+                    fullfill();
+                });
+            });
+        });
+    }
+
+    function initControllers() {
+        return new Promise(function (fullfill){
+            Member.init(db);
+            fullfill();
+        });
+    }
+
+    function initRouter() {
+        return new Promise(function (fullfill) {
+            router = new Router();
+            router.load(config.files.routes).then(function () {
+                fullfill();
+            });
+        });
     }
 
     function handleConnections() {
