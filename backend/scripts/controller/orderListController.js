6f12cf605962516d30e11ac5d5b86989bb5c5543
John Huang <johnhuang@kkbox.com>
2014-08-02 17:25:23 +0800

    [Backend] Fix orderlist.

--- backend/scripts/controller/orderListController.js
+++ backend/scripts/controller/orderListController.js
@@ -5,13 +5,16 @@ angular.module('BackendHimawari')
                 $scope.orders = data;
             })
             .then(function () {
-                return api.get('/members/');
-            })
-            .then(function () {
                 return api.get('/menus/');
             })
             .then(function (data) {
+                $scope.menu = data;
+                console.log(data);
+                return api.get('/members/');
+            })
+            .then(function (data) {
                 $scope.member = data;
+                console.log(data);
                 $scope.member_map = {}
                 for (var i in data) {
                     $scope.member_map[data[i].username] = data[i];
