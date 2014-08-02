2ffc56af75b586d96813b111b28f03f2404c60e2
John Huang <johnhuang@kkbox.com>
2014-08-02 23:48:51 +0800

    [Backend] Add basic style for order-list And change color scheme.

--- backend/scripts/controller/orderListController.js
+++ backend/scripts/controller/orderListController.js
@@ -3,6 +3,7 @@ angular.module('BackendHimawari')
         api.get('/orders/current/')
             .then(function (data) {
                 $scope.orders = data;
+                console.log(data);
             })
             .then(function () {
                 return api.get('/menus/');
@@ -19,6 +20,18 @@ angular.module('BackendHimawari')
                 for (var i in data) {
                     $scope.member_map[data[i].username] = data[i];
                 }
+                var rows = [], row = [], idx = 0;
+                for (var k in $scope.orders) {
+                    row.push($scope.orders[k]);
+                    console.log($scope.orders[k]);
+                    if ((idx + 1) % 5 == 0) {
+                        rows.push(row);
+                        row = [];
+                    }
+                    idx++;
+                }
+                rows.push(row);
+                $scope.rows = rows;
                 $scope.$apply();
             });
     }]);
